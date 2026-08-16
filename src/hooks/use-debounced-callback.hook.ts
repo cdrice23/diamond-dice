cc501d5aba875b0d082e29100986f36d90659a1d
Chris Rice <cd.rice23@gmail.com>
2026-08-16 09:00:59 -0700

    Update useDebouncedCallback to remove any typing and use generics instead.

--- src/hooks/use-debounced-callback.hook.ts
+++ src/hooks/use-debounced-callback.hook.ts
@@ -1,6 +1,6 @@
 import { useCallback, useRef } from 'react';
 
-export function useDebouncedCallback<T extends (...args: any[]) => void>(callback: T, delay: number) {
+export function useDebouncedCallback<T extends (...args: Parameters<T>) => void>(callback: T, delay: number) {
   const timer = useRef<ReturnType<typeof setTimeout> | undefined>(undefined);
   return useCallback((...args: Parameters<T>) => {
     clearTimeout(timer.current);
